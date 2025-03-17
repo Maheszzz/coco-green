142f54a6ac4b188be369fbbdd5956dc5541c5052
maheszzz <maheswaran2527@gmail.com>
2025-03-17 23:15:13 +0530

    Remove wishlist button functionality and associated code

--- script.js
+++ script.js
@@ -64,7 +64,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const thumbnails = document.querySelectorAll('.thumbnail');
   const mainImage = document.getElementById('mainImage');
-  const wishlistBtn = document.querySelector('.wishlist-btn');
 
   // Set up thumbnail click behavior
   thumbnails.forEach(thumbnail => {
@@ -79,18 +78,6 @@ document.addEventListener("DOMContentLoaded", function () {
       const imgSrc = this.getAttribute('data-img');
       mainImage.src = imgSrc;
     });
-  });
-
-  // Toggle wishlist button
-  wishlistBtn.addEventListener('click', function() {
-    const svg = this.querySelector('svg');
-    if (svg.getAttribute('fill') === '#c2c2c2') {
-      svg.setAttribute('fill', '#ff6161');
-      svg.innerHTML = '<path d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>';
-    } else {
-      svg.setAttribute('fill', '#c2c2c2');
-      svg.innerHTML = '<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>';
-    }
   });
 });
 
