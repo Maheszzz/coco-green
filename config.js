cca72a603db165edf9268d1090df330e66f1130a
maheszzz <maheswaran2527@gmail.com>
2025-03-28 14:14:12 +0530

    configfinal

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const config = {
   emailjs: {
     serviceId: "YOUR_SERVICE_ID",
     templateId: "YOUR_TEMPLATE_ID",
-    userId: "9tY__hTImYmuy84ZM", // Already set in HTML
+    userId: "YOURUSERID", // Already set in HTML
   },
 };
